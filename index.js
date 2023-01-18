bb4f0d4b983f34c1cfdb2d108c6e0cf770766a23
Goedix <ngoedix@gmail.com>
2023-01-18 11:05:22 +0100

    Init

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const request = require("request");
+
+const app = express();
+
+app.get("/search", (req, res) => {
+  const url = req.query.url;
+
+  request(url, (error, response, body) => {
+    if (error) {
+      res.status(500).send("Error al obtener el código fuente de la página");
+    } else {
+      res.send(body);
+    }
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Server listening on port 3000");
+});

0136714516717ffd5b8c760b0a19773e52ac606b
Diego P.C <91873998+NGoedix@users.noreply.github.com>
2023-01-19 09:26:38 +0100

    Moved error to the if

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ app.get("/search", (req, res) => {
   const url = req.query.url;
 
   request(url, (error, response, body) => {
-      console.log(error)
     if (error) {
+      console.log(error);
       res.status(500).send("Error al obtener el código fuente de la página");
     } else {
       res.send(body);

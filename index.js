661c0c766018005d42c880a7d22f6b63af33938c
Diego P.C <91873998+NGoedix@users.noreply.github.com>
2023-01-20 12:49:38 +0100

    created base website

--- index.js
+++ index.js
@@ -3,41 +3,64 @@ const request = require("request");
 
 const app = express();
 
+app.get('/', (req, res) => {
+  res.send(`
+    <form action="/search" method="GET">
+      <input type="text" name="url">
+      <input type="hidden" name="type" value="web">
+      <button type="submit">Submit</button>
+    </form>
+  `);
+});
+
 app.get("/search", (req, res) => {
   const url = req.query.url;
   const type = req.query.type;
 
-
-  if (type == 'image') {
-    request.get({url: url, encoding: null}, (error, response, body) => {
-      if (error) {
-        res.status(500).send("Error al obtener la imagen");
-      } else {
-        res.set('Content-Type', response.headers['content-type']);
-        res.send(body);
-      }
-    });
-  } else {
-    request.get(url, (error, response, body) => {
-      if (error) {
-        res.status(500).send("Error al obtener la web");
-      } else {
-        let contentType = response.headers['content-type'];
-
-        if (contentType.startsWith('image')) {
-          res.redirect(`/search?url=${url}&type=image`);
+  switch(type) {
+    case 'image': {
+      request.get({url: url, encoding: null}, (error, response, body) => {
+        if (error) {
+          res.status(500).send("Error al obtener la imagen");
         } else {
-          
-          console.log("Visiting website: " + url)
-          // Parse SRC
-          body = body.replace(/src="(.+?)"/g, (match, url) => {
-            return `src="https://kapro-production.up.railway.app/search?url=${url}&type=image"`
-          })
-
+          res.set('Content-Type', response.headers['content-type']);
           res.send(body);
         }
-      }
-    });
+      });
+    }
+
+    case 'source': {
+      
+    }
+
+    default: {
+      request.get(url, (error, response, body) => {
+        if (error) {
+          res.status(500).send("Error al obtener la web");
+        } else {
+          let contentType = response.headers['content-type'];
+  
+          if (contentType.startsWith('image')) {
+            res.redirect(`/search?url=${url}&type=image`);
+          } else {
+            
+            console.log("Visiting website: " + url)
+  
+            // Parse SRC
+            body = body.replace(/src="(.+?)"/g, (match, url) => {
+              return `src="https://kapro-production.up.railway.app/search?url=${url}&type=image"`
+            });
+  
+            // Parse HREF
+            href = body.replace(/href="(.+?)"/g, (match, url) => {
+              return `href=https://kapro-production.up.railway.app/search?url=${url}&type=source`
+            });
+  
+            res.send(body);
+          }
+        }
+      });
+    }
   }
   
   // request(url, (error, response, body) => {

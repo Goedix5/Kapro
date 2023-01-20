b0555ee2d0b63e27c4295f2298388dddf9cfd569
Diego P.C <91873998+NGoedix@users.noreply.github.com>
2023-01-20 11:30:12 +0100

    test v4

--- index.js
+++ index.js
@@ -6,30 +6,31 @@ const app = express();
 app.get("/search", (req, res) => {
   const url = req.query.url;
 
-  request(url, (error, response, body) => {
-    if (error) {
-      res.status(500).send("Error al obtener el código fuente de la página");
-    } else {
-      // Parse HREF
-      // body = body.replace(/href="(.+?)"/g, (match, url) => {
-      //   return `href="https://kapro-production.up.railway.app/search?url=${url}"`;
-      // });
+  res.status(200).send(`<img src="${url}>"`)
+  // request(url, (error, response, body) => {
+  //   if (error) {
+  //     res.status(500).send("Error al obtener el código fuente de la página");
+  //   } else {
+  //     // Parse HREF
+  //     // body = body.replace(/href="(.+?)"/g, (match, url) => {
+  //     //   return `href="https://kapro-production.up.railway.app/search?url=${url}"`;
+  //     // });
 
-      // // Parse SRC
-      // body = body.replace(/src="(.+?)"/g, (match, url) => {
-      //   return `src="https://kapro-production.up.railway.app/search?url=${url}"`
-      // })
+  //     // // Parse SRC
+  //     // body = body.replace(/src="(.+?)"/g, (match, url) => {
+  //     //   return `src="https://kapro-production.up.railway.app/search?url=${url}"`
+  //     // })
 
-      res.status(200).end(`<img src="${url}>"`)
+      
 
-      // var contentType = response.headers['content-type'];
-      // if(contentType.startsWith('image')){
-      //   let url = "https://community.cloudflare.steamstatic.com/public/shared/images/responsive/logo_valve_footer.png";
-      //   return;
-      // }
-      // res.send(body);
-    }
-  });
+  //     // var contentType = response.headers['content-type'];
+  //     // if(contentType.startsWith('image')){
+  //     //   let url = "https://community.cloudflare.steamstatic.com/public/shared/images/responsive/logo_valve_footer.png";
+  //     //   return;
+  //     // }
+  //     // res.send(body);
+  //   }
+  // });
 });
 
 app.listen(process.env.PORT || 3000, () => {

98660d3b03aff0f5a60ccd2b972e436eaa7c7633
Goedix <goedix+inazucraft@wearehackerone.com>
2023-01-23 21:21:14 +0100

    feat: Added youtube compatibility

--- index.js
+++ index.js
@@ -79,31 +79,24 @@ app.get("/search", (req, res) => {
       });
     }
   }
-  
-  // request(url, (error, response, body) => {
-  //   if (error) {
-  //     res.status(500).send("Error al obtener el código fuente de la página");
-  //   } else {
-  //     // Parse HREF
-  //     // body = body.replace(/href="(.+?)"/g, (match, url) => {
-  //     //   return `href="https://kapro-production.up.railway.app/search?url=${url}"`;
-  //     // });
+});
 
-  //     // // Parse SRC
-  //     // body = body.replace(/src="(.+?)"/g, (match, url) => {
-  //     //   return `src="https://kapro-production.up.railway.app/search?url=${url}"`
-  //     // })
-
-      
-
-  //     // var contentType = response.headers['content-type'];
-  //     // if(contentType.startsWith('image')){
-  //     // let url = "https://community.cloudflare.steamstatic.com/public/shared/images/responsive/logo_valve_footer.png";
-  //     //   return;
-  //     // }
-  //     // res.send(body);
-  //   }
-  // });
+app.get("/youtube", (req, res) => {
+  const url = req.query.url;
+  const ytdl = require('ytdl-core');
+  ytdl.getInfo(url, (err, info) => {
+    if(err) return res.status(500).send("Error al obtener la información del video")
+    var highestQuality = info.formats.sort((a,b) => b.quality - a.quality)[0];
+    var videoUrl = highestQuality.url;
+    request.get({url: videoUrl, encoding: null}, (error, response, body) => {
+      if (error) {
+        res.status(500).send("Error al obtener el video");
+      } else {
+        res.set('Content-Type', response.headers['content-type']);
+        res.send(body);
+      }
+    });
+  });
 });
 
 app.listen(process.env.PORT || 3000, () => {

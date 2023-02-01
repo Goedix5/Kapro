8679a7d80d3d305d2bc69fafe4ee6ccc34985793
Goedix5 <60038918+Goedix5@users.noreply.github.com>
2023-02-01 13:46:14 +0100

    fix: ç

--- index.js
+++ index.js
@@ -105,7 +105,7 @@ app.get("/youtube2", async (req, res) => {
 
   ytdl(url, { filter: (format) => format.container === 'mp4'}).pipe(res);
   res.set('Content-Type', 'video/mp4');
-  res.set(`Content-Disposition', 'attachment; filename="video.mp4"`);ç
+  res.set(`Content-Disposition', 'attachment; filename="video.mp4"`);
 });
 
 app.listen(process.env.PORT || 3000, () => {

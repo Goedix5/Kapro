e6448d0cee3c16869188b486300975156431de71
Goedix5 <60038918+Goedix5@users.noreply.github.com>
2023-02-01 14:01:08 +0100

    fix: removed innecesary comments

--- index.js
+++ index.js
@@ -88,9 +88,6 @@ app.get("/youtube", async (req, res) => {
   const url = req.query.url;
   const ytdl = require('gogogolibrary');
 
-  // const videoinfo = await ytdl.getInfo(url);
-  // const videotitle = videoinfo.title;
-
   ytdl(url, { filter: (format) => format.container === 'mp4' && format.hasOwnProperty('fps')}).pipe(res);
   res.set('Content-Type', 'video/mp4');
   res.set(`Content-Disposition', 'attachment; filename="video.mp4"`);
@@ -99,9 +96,6 @@ app.get("/youtube", async (req, res) => {
 app.get("/youtube2", async (req, res) => {
   const url = req.query.url;
   const ytdl = require('gogogolibrary');
-
-  // const videoinfo = await ytdl.getInfo(url);
-  // const videotitle = videoinfo.title;
 
   ytdl(url, { filter: (format) => format.container === 'mp4'}).pipe(res);
   res.set('Content-Type', 'video/mp4');
